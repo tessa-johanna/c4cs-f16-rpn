43e275fcf6609441f535ada4f84fa0c38e3cfc34
tessajo <tessajo@umich.edu>
2016-10-12 16:18:38 -0400

    Testcase for carat added

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,6 @@ class TestBasics(unittest.TestCase):
 	def test_toomanythings(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
+	def test-_carat(self):
+		result = rpn.calculate("3 2 ^")
+		self.assertEqual(9, result)
